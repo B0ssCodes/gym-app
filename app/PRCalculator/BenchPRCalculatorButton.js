0d3b23e28188727efeb56b0bf9951d9f00ed765d
Boss Codes <jadsaderkarate@gmail.com>
2023-10-26 17:21:01 +0300

    joeBug

--- app/PRCalculator/BenchPRCalculatorButton.js
+++ app/PRCalculator/BenchPRCalculatorButton.js
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default function BenchPRCalculatorButton() {
     return (
-        <Link href="/PRCalculator" className='btn bg-[#0a3a2a] text-[#FAF9F6] hover:bg-emerald-900 rounded-3xl' >Check out our Bench Program Maker</Link>
+        <Link href="/BenchProgram" className='btn bg-[#0a3a2a] text-[#FAF9F6] hover:bg-emerald-900 rounded-3xl' >Check out our Bench Program Maker</Link>
     )
 }
